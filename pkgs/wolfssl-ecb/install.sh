106931ec449ed350bfe97f9f18817e4614079a18
Michele Beretta <beretta.m98@gmail.com>
2024-07-01 09:32:35 +0200

    More automated install script

--- pkgs/wolfssl-ecb/install.sh
+++ pkgs/wolfssl-ecb/install.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 srcdir="$(pwd)/src"
+[ -d "$srcdir" ] || mkdir -p "$srcdir"
 
 source "./PKGBUILD"
+
+parts=(${source//::/ })
+
+out="${parts[0]}"
+if [ -f "$out" ]; then
+  echo "Sources already downloaded"
+else
+  wget -O "$out" "${parts[1]}"
+  tar -xvf "$out" -C "$srcdir"
+fi
 
 build
 check
