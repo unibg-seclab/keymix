bfb1ebd786ac34e8215cc61c1430ab01af618578
Michele Beretta <beretta.m98@gmail.com>
2024-07-01 09:43:31 +0200

    Unused library

--- aesni.h
+++ aesni.h
@@ -4,7 +4,6 @@
 #include "types.h"
 #include <assert.h>
 #include <stdlib.h>
-#include <wolfssl/wolfcrypt/aes.h>
 
 int aesni(byte *seed, byte *out, size_t seed_size, unsigned int blocks_per_macro);
 
