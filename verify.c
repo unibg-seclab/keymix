a48924845c9acdc58deb6a2f4a18bf5fd68801aa
Dario Facchinetti <dario.fad@gmail.com>
2024-07-13 06:48:37 +0200

    Verify: avoid random values and improve visualization

--- verify.c
+++ verify.c
@@ -6,39 +6,71 @@
 #include <string.h>
 
 void print_buffer(byte data[], size_t size, size_t fanout) {
+        unsigned int addr = 0;
+        printf("%d\t", addr);
         for (int i = 0; i < size; i++) {
                 printf("%02x", data[i]);
-                if ((i + 1) % (SIZE_MACRO / fanout) == 0)
+                if ((i + 1) % (SIZE_MACRO / fanout) == 0) {
                         printf(" | ");
-                if ((i + 1) % SIZE_MACRO == 0)
+                        addr++;
+                }
+                if ((i + 1) % SIZE_MACRO == 0) {
                         printf("\n");
+                        printf("%d\t", addr);
+                }
         }
+        printf("\n");
 }
 
 int main() {
-        srand(time(NULL));
 
-        size_t fanout = 3;
-        size_t level  = 2;
-        size_t size   = (size_t)pow(fanout, level) * SIZE_MACRO;
+        size_t diff_factor = 3;
+        size_t level       = 3;
+        size_t size        = (size_t)pow(diff_factor, level) * SIZE_MACRO;
         byte in[size];
         byte *out_a = malloc(size);
         byte *out_b = malloc(size);
+        byte *out_c = malloc(size);
 
+        unsigned int j = 0;
         for (int i = 0; i < size; i++) {
-                in[i]    = rand() % 256;
+                if (i % 16 == 0)
+                        j++;
+                in[i]    = j;
                 out_a[i] = 0;
                 out_b[i] = 0;
+                out_c[i] = 0;
         }
 
-        shuffle(out_a, in, size, level, fanout);
-        // shuffle_opt(out_b, in, size, level, fanout);
-        shuffle_opt2(out_b, in, size, level, fanout);
+        for (unsigned int l = 0; l < level; l++) {
+                printf("INPUT\n");
+                print_buffer(in, size, diff_factor);
 
-        if (memcmp(out_a, out_b, size) != 0)
-                return 1;
+                swap(out_a, in, size, l, diff_factor);
+                printf("OUT_A\n");
+                print_buffer(out_a, size, diff_factor);
+
+                shuffle(out_b, in, size, l, diff_factor);
+                printf("OUT_B\n");
+                print_buffer(out_b, size, diff_factor);
+
+                shuffle_opt2(out_c, in, size, l, diff_factor);
+                printf("OUT_C\n");
+                print_buffer(out_c, size, diff_factor);
+
+                if (memcmp(out_a, out_b, size) != 0) {
+                        printf("out_a differs from out_b\n");
+                        return 1;
+                }
+                if (memcmp(out_a, out_c, size) != 0) {
+                        printf("out_a differs from out_c\n");
+                        return 1;
+                }
+        }
 
         free(out_a);
         free(out_b);
+        free(out_c);
+
         return 0;
 }
