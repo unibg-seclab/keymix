dfd05978ba69312b973d4b6fc0344fca0cb330e8
Michele Beretta <beretta.m98@gmail.com>
2024-07-29 16:28:40 +0200

    Remove file_encrypt, as fread arguments have no effect on performance

--- src/file.c
+++ src/file.c
@@ -55,53 +55,6 @@ void derive_thread_numbers(uint8_t *internal_threads, uint8_t *external_threads,
         *external_threads = ethr;
 }
 
-// I'll leave this here for now, even if unused, because we might want
-// to come back and read with different sizes if it proves faster than
-// supporting a stream
-int file_encrypt(FILE *fout, FILE *fin, keymix_ctx_t *ctx, uint8_t threads) {
-        uint8_t internal_threads, external_threads;
-
-        // First, separate threads into internals and externals correctly,
-        // since internals must be a power of fanout
-        derive_thread_numbers(&internal_threads, &external_threads, ctx->fanout, threads);
-
-        // Then, we encrypt the input resource in a "streamed" manner:
-        // that is, we read `external_threads` groups, each one of size
-        // `ctx->key_size`, use encrypt_t on that, and lastly write the result
-        // to the output.
-
-        // However, note that the resource could be not a multiple of key_size,
-        // hence we have to read the MINIMUM between `external_threads * key_size`
-        // and the remaining resource, which is why we track input_size
-
-        size_t buffer_size    = external_threads * ctx->key_size;
-        size_t remaining_size = get_file_size(fin);
-        byte *in_buffer       = malloc(buffer_size);
-        byte *out_buffer      = malloc(buffer_size);
-
-        uint128_t counter = 0;
-
-        while (remaining_size > 0) {
-                size_t size_to_read = MIN(remaining_size, buffer_size);
-
-                fread(in_buffer, size_to_read, 1, fin);
-
-                encrypt_ex(ctx, in_buffer, out_buffer, size_to_read, external_threads,
-                           internal_threads, counter);
-
-                fwrite(out_buffer, size_to_read, 1, fout);
-
-                remaining_size -= size_to_read;
-                counter += external_threads; // We encrypt `external_threads` at a time
-        }
-
-        free(in_buffer);
-        return 0;
-}
-
-// This is the same, but with support for streams, that is, when we do not
-// have a way to get the input's size.
-// Note that this works for traditional files too.
 int stream_encrypt(FILE *fout, FILE *fin, keymix_ctx_t *ctx, uint8_t threads) {
         uint8_t internal_threads, external_threads;
 
