944be6828440214370aced1a77e09ab2d90bad0e
Michele Beretta <beretta.m98@gmail.com>
2024-08-05 10:38:49 +0200

    First draft of graphs for keymix

--- graphs/common.py
+++ graphs/common.py
@@ -1,5 +1,22 @@
+import matplotlib
+
+matplotlib.rc('font', size=18)
+matplotlib.rc('pdf', fonttype=42)
+
 def to_mib(bytes):
     return bytes / 1024 / 1024
 
 def to_sec(ms):
     return ms / 1000
+
+def pltlegend(plt, labels):
+    x0, y0 = -0.09, 1.02
+    width = 1.15
+    height = 0.2
+    plt.legend(labels,
+               frameon=False,
+               mode='expand',
+               bbox_to_anchor=(x0, y0, width, height),
+               ncol=2,
+               handlelength=1,
+               loc="lower left")
--- graphs/keymix.py
+++ graphs/keymix.py
@@ -11,47 +11,67 @@ implementations = list(df.implementation.unique())
 markers = ['*', 'o', 'x']
 fanouts = list(df.fanout.unique())
 
+def derive_data(df, impl, fanout, threads):
+    data = df[(df.implementation == impl) & (df.fanout == fanout) & (df.internal_threads == threads)]
+    data = data.groupby('key_size', as_index=False).agg({'time': ['mean', 'std']})
+    data.columns = ['key_size', 'time_mean', 'time_std']
+    data.reindex(columns=data.columns)
+    return data
+
 for fanout in fanouts:
+    match fanout:
+        case 2:
+            threads = 8
+        case 3:
+            threads = 9
+        case 4:
+            threads = 4
+
     # --------------------------- Time
 
     plt.figure()
-    plt.title(f'Keymix time (fanout {fanout})')
+    # plt.title(f'Keymix time (fanout {fanout})')
 
     for m, impl in zip(markers, implementations):
-        data = df[(df.implementation == impl) & (df.fanout == fanout) & (df.internal_threads == fanout)]
-        data = data.groupby('key_size', as_index=False).agg({'time': ['mean', 'std']})
-        data.columns = ['key_size', 'time_mean', 'time_std']
-        data.reindex(columns=data.columns)
-        # print(data)
-
+        data = derive_data(df, impl, fanout, 1)
         xs = [to_mib(x) for x in data.key_size]
         ys = [to_sec(y) for y in data.time_mean]
         plt.loglog(xs, ys, marker=m)
 
+    for m, impl in zip(markers, implementations):
+        data = derive_data(df, impl, fanout, threads)
+        xs = [to_mib(x) for x in data.key_size]
+        ys = [to_sec(y) for y in data.time_mean]
+        plt.loglog(xs, ys, marker=m, linestyle='dashed')
 
-    plt.legend(implementations)
+
+    plt.legend(implementations + [f'{impl} ({threads} threads)' for impl in implementations])
     plt.xlabel('Key size [MiB]')
     plt.ylabel('Average time [s]')
-    plt.savefig(f'graphs/by-fanout-{fanout}-time.pdf')
+    plt.ylim(1e1, 1e3)
+    plt.savefig(f'graphs/keymix-f{fanout}-time.pdf', bbox_inches='tight')
 
     # --------------------------- speed
 
     plt.figure()
-    plt.title(f'Keymix speed (fanout {fanout})')
+    # plt.title(f'Keymix speed (fanout {fanout})')
 
     for m, impl in zip(markers, implementations):
-        data = df[(df.implementation == impl) & (df.fanout == fanout) & (df.internal_threads == fanout)]
-        data = data.groupby('key_size', as_index=False).agg({'time': ['mean', 'std']})
-        data.columns = ['key_size', 'time_mean', 'time_std']
-        data.reindex(columns=data.columns)
-        # print(data)
-
+        data = derive_data(df, impl, fanout, 1)
         xs = [to_mib(x) for x in data.key_size]
         ys = [x / to_sec(y) for x, y in zip(xs, data.time_mean)]
         plt.loglog(xs, ys, marker=m)
 
 
-    plt.legend(implementations)
+    for m, impl in zip(markers, implementations):
+        data = derive_data(df, impl, fanout, threads)
+        xs = [to_mib(x) for x in data.key_size]
+        ys = [x / to_sec(y) for x, y in zip(xs, data.time_mean)]
+        plt.loglog(xs, ys, marker=m, linestyle='dashed')
+
+
+    pltlegend(plt, implementations + [f'{impl} ({threads} threads)' for impl in implementations])
     plt.xlabel('Key size [MiB]')
     plt.ylabel('Average speed [MiB/s]')
-    plt.savefig(f'graphs/by-fanout-{fanout}-speed.pdf')
+    plt.ylim(1e1, 1e3)
+    plt.savefig(f'graphs/keymix-f{fanout}-speed.pdf', bbox_inches='tight')
