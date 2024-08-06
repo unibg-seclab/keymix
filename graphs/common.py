d26d7fbc94b708b9cdd2f8479d30b4468a909ad8
Matthew Rossi <mattw.rossi@gmail.com>
2024-08-06 15:30:58 +0200

    Update legend display

--- graphs/common.py
+++ graphs/common.py
@@ -9,14 +9,14 @@ def to_mib(bytes):
 def to_sec(ms):
     return ms / 1000
 
-def pltlegend(plt, labels, ncol=2):
-    x0, y0 = 0, 1.02
-    width = 1
+def pltlegend(plt, labels):
+    x0, y0 = -0.14, 1.02
+    width = 1.17
     height = 0.2
     plt.legend(labels,
                frameon=False,
                mode='expand',
                bbox_to_anchor=(x0, y0, width, height),
-               ncol=ncol,
-               handlelength=1,
+               ncol=len(labels) // 2,
+               handlelength=1.5,
                loc="lower left")
--- graphs/keymix.py
+++ graphs/keymix.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from itertools import product
+
 from common import *
 
 FILE = 'data/out-preliminary.csv'
@@ -9,7 +11,7 @@ df = pd.read_csv(FILE)
 
 implementations = ['openssl', 'wolfssl']
 impl_legend= ['OpenSSL', 'wolfSSL']
-markers = ['*', 'o', 'x']
+markers = ['*', 'o']
 linestyles = ['solid', 'dashed', 'dotted']
 fanouts = list(df.fanout.unique())
 
@@ -29,24 +31,19 @@ for fanout in fanouts:
         case 4:
             threads = [1, 4]
 
-    legend = []
-    for impl in impl_legend:
-        for t in threads:
-            entry = impl if t == 1 else f'{impl} ({t} threads)'
-            legend.append(entry)
+    legend = [f'{impl}' if thr == 1 else f'{impl} ({thr} threads)' for thr, impl in product(threads, impl_legend)]
 
     # --------------------------- Time
 
     plt.figure()
     # plt.title(f'Keymix time (fanout {fanout})')
 
-    for m, impl in zip(markers, implementations):
-        for t, style in zip(threads, linestyles):
+    for style, t in zip(linestyles, threads):
+        for m, impl in zip(markers, implementations):
             data = derive_data(df, impl, fanout, t)
             xs = [to_mib(x) for x in data.key_size]
             ys = [to_sec(y) for y in data.time_mean]
             plt.loglog(xs, ys, marker=m, linestyle=style)
-
 
     pltlegend(plt, legend)
     plt.xlabel('Key size [MiB]')
@@ -59,13 +56,12 @@ for fanout in fanouts:
     plt.figure()
     # plt.title(f'Keymix speed (fanout {fanout})')
 
-    for m, impl in zip(markers, implementations):
-        for t, style in zip(threads, linestyles):
+    for style, t in zip(linestyles, threads):
+        for m, impl in zip(markers, implementations):
             data = derive_data(df, impl, fanout, t)
             xs = [to_mib(x) for x in data.key_size]
             ys = [x / to_sec(y) for x, y in zip(xs, data.time_mean)]
             plt.loglog(xs, ys, marker=m, linestyle=style)
-
 
     pltlegend(plt, legend)
     plt.xlabel('Key size [MiB]')
