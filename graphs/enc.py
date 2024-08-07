64f52f8fb6541d8a371c7fce00b79a7bd5725141
Michele Beretta <beretta.m98@gmail.com>
2024-08-07 16:56:14 +0200

    Add some encryption graphs

--- graphs/enc.py
+++ graphs/enc.py
@@ -9,27 +9,23 @@ from common import *
 FILE = 'data/enc.csv'
 
 df = pd.read_csv(FILE)
+df['outsize_mib'] = to_mib(df.outsize)
 
-implementations = ['openssl', 'wolfssl']
-impl_legend= ['OpenSSL', 'wolfSSL']
+# Remove 5* MiB files
+df = df[(df.outsize_mib != 5) & (df.outsize_mib != 50) & (df.outsize_mib != 500) & (df.outsize_mib != 5120)]
+
+implementations = ['openssl', 'wolfssl', 'aesni']
+impl_legend= ['OpenSSL', 'wolfSSL', 'AES-NI']
 fanouts = list(df.fanout.unique())
 
-if 'aesni' in implementations:
-    ytop_speed = 2000
-else:
-    ytop_speed = 1100
+# ----------------------------------------- "Traditional" encryption curves
 
 for impl, name in zip(implementations, impl_legend):
     for fanout in fanouts:
-        match fanout:
-            case 2: threads = 8
-            case 3: threads = 9
-            case 4: threads = 4
-
         plt.figure()
-        plt.title(f'Encryption times, {name} (fanout {fanout})')
+        # plt.title(f'Encryption times, {name} (fanout {fanout})')
 
-        data = df[(df.fanout == fanout) & (df.internal_threads == threads) & (df.external_threads == 8)]
+        data = df[(df.fanout == fanout) & (df.internal_threads == 1) & (df.external_threads == 1)]
         data = data[data.implementation == impl]
 
         key_sizes = data.key_size.unique()
@@ -48,9 +44,9 @@ for impl, name in zip(implementations, impl_legend):
         plt.savefig(f'graphs/enc-f{fanout}-{impl}-time.pdf')
 
         plt.figure()
-        plt.title(f'Encryption speeds, {name} (fanout {fanout})')
+        # plt.title(f'Encryption speeds, {name} (fanout {fanout})')
 
-        data = df[(df.fanout == fanout) & (df.internal_threads == threads) & (df.external_threads == 8)]
+        data = df[(df.fanout == fanout) & (df.internal_threads == 1) & (df.external_threads == 1)]
         data = data[data.implementation == impl]
 
         key_sizes = data.key_size.unique()
@@ -62,8 +58,32 @@ for impl, name in zip(implementations, impl_legend):
             plt.plot(xs, ys, marker='o')
 
         plt.xscale('log')
-        plt.ylim(top=ytop_speed)
         plt.legend([f'{round(to_mib(k), 2)} MiB' for k in key_sizes])
         plt.xlabel('File size [MiB]')
         plt.ylabel('Average speed [MiB/s]')
         plt.savefig(f'graphs/enc-f{fanout}-{impl}-speed.pdf')
+
+# ----------------------------------------- Multi-threading improvements
+
+for fanout in fanouts:
+    match fanout:
+        case 2: size = 201326592
+        case 3: size = 229582512
+        case 4: size = 201326592
+
+    plt.figure()
+
+    data = df[(df.fanout == fanout) & (df.internal_threads == 1) & (df.key_size == size)]
+
+    for impl in implementations:
+        grouped = df_groupby(data[data.implementation == impl], 'external_threads')
+        xs = list(grouped.external_threads)
+        ys = [to_mib(size) / to_sec(y) for y in grouped.time_mean]
+        plt.plot(xs, ys, marker='o')
+
+    pltlegend(plt, impl_legend)
+    plt.xticks(xs)
+    plt.xlabel('Number of threads')
+    plt.ylabel('Average speed [MiB/s]')
+    plt.ylim(0, 85)
+    plt.savefig(f'graphs/enc-f{fanout}-threading-speed.pdf')
