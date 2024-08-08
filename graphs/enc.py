adada64ff35068b758af37197f6affddd5d535ae
Matthew Rossi <mattw.rossi@gmail.com>
2024-08-08 17:45:53 +0200

    Use multiple markers in enc

--- graphs/enc.py
+++ graphs/enc.py
@@ -11,9 +11,11 @@ df['outsize_mib'] = to_mib(df.outsize)
 # Remove 5* MiB files
 df = df[(df.outsize_mib != 5) & (df.outsize_mib != 50) & (df.outsize_mib != 500) & (df.outsize_mib != 5120)]
 
-implementations = ['aesni']
-impl_legend= ['AES-NI']
-fanouts = [3]
+implementations = ['aesni'] # ['aesni', 'openssl', 'wolfssl']
+impl_legend= ['AES-NI'] # ['AES-NI', 'OpenSSL', 'wolfSSL']
+fanouts = [3] # [2, 3, 4]
+
+markers = ["o", "s", "^", "D", "*", "p", "h", "8", "v"]
 
 # ----------------------------------------- "Traditional" encryption curves
 
@@ -28,11 +30,11 @@ for impl, name in zip(implementations, impl_legend):
         key_sizes = data.key_size.unique()
         key_sizes_in_mib = [to_mib(k) for k in key_sizes]
 
-        for size in key_sizes:
+        for i, size in enumerate(key_sizes):
             grouped = df_groupby(data[data.key_size == size], 'outsize')
             xs = [to_mib(x) for x in grouped.outsize]
             ys = [to_sec(y) for y in grouped.time_mean]
-            plt.plot(xs, ys, marker='o')
+            plt.plot(xs, ys, marker=markers[i])
 
         plt.xscale('log')
         # plt.ylim(top=120)
@@ -49,11 +51,11 @@ for impl, name in zip(implementations, impl_legend):
         data = df[(df.fanout == fanout) & (df.internal_threads == 1) & (df.external_threads == 1)]
         data = data[data.implementation == impl]
 
-        for size in key_sizes:
+        for i, size in enumerate(key_sizes):
             grouped = df_groupby(data[data.key_size == size], 'outsize')
             xs = [to_mib(x) for x in grouped.outsize]
             ys = [x / to_sec(y) for x, y in zip(xs, grouped.time_mean)]
-            plt.plot(xs, ys, marker='o')
+            plt.plot(xs, ys, marker=markers[i])
 
         plt.xscale('log')
         # plt.ylim(top=75)
@@ -80,11 +82,11 @@ for fanout in fanouts:
 
     data = df[(df.fanout == fanout) & (df.internal_threads == 1) & (df.outsize == outsize) & (df.key_size == size)]
 
-    for impl in implementations:
+    for i, impl in enumerate(implementations):
         grouped = df_groupby(data[data.implementation == impl], 'external_threads')
         xs = list(grouped.external_threads)
         ys = [to_mib(size) / to_sec(y) for y in grouped.time_mean]
-        plt.plot(xs, ys, marker='o')
+        plt.plot(xs, ys, marker=markers[i])
 
     pltlegend(plt, impl_legend)
     plt.xticks(xs)
