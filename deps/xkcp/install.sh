e491d6111d7e356ef0bce604dcb54e63acdbefa4
Matthew Rossi <mattw.rossi@gmail.com>
2025-01-31 12:10:47 +0100

    Fix dependency install scripts

--- deps/xkcp/install.sh
+++ deps/xkcp/install.sh
@@ -4,11 +4,11 @@ XKCP_TARGET="AVX2"
 echo "[*] Look into https://github.com/XKCP/XKCP for the best compilation target for your cpu architecture (default: AVX2)"
 cd $projroot/deps/XKCP
 git submodule update --init
-make $(XKCP_TARGET)/libXKCP.so
+make $XKCP_TARGET/libXKCP.so
 echo "[*] Installing XKCP library in /usr/local/lib ..."
 sudo mkdir -p /usr/local/include/xkcp
-sudo cp -r bin/$(XKCP_TARGET)/libXKCP.so.headers/* /usr/local/include/xkcp
-sudo cp bin/$(XKCP_TARGET)/libXKCP.so /usr/local/lib/libXKCP.so
+sudo cp -r bin/$XKCP_TARGET/libXKCP.so.headers/* /usr/local/include/xkcp
+sudo cp bin/$XKCP_TARGET/libXKCP.so /usr/local/lib/libXKCP.so
 echo "[*] Updating shared library cache ..."
 sudo ldconfig
 cd -
